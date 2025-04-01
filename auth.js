95c893da5791e2c37bb1a2e3b55b30107dc76e22
VrzIgor <igor.varzanov@yandex.ru>
2025-04-01 21:10:49 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -2,9 +2,9 @@ function checkPassword() {
     const password = document.getElementById("password").value;
     const errorElement = document.getElementById("error");
     
-    // Замените "мой_пароль" на свой реальный пароль
-    if (password === "мой_пароль") {
-        window.location.href = "index.html"; // Успешный вход
+    if (password === "3647") {
+        sessionStorage.setItem("authenticated", "true");
+        window.location.href = "index.html";
     } else {
         errorElement.textContent = "Неверный пароль!";
     }
